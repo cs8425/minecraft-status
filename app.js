a3bbdb878c77f2ae54e7a6fff1b1f669d6f3d9eb
cs8425 <cs8425@gmail.com>
2015-04-17 17:15:55 +0800

    clean up all about minecraft, leave message board only

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 var cluster = require('cluster');
-var net = require('net');
-var child_process = require('child_process');
-var spawn = child_process.spawn;
 
 var config = {
-	port: 8080,
-	helper: '/home/pi/spigot_server/minecraft',
-	mcport: 25565,
+	port: 8080
 };
 
 if (cluster.isMaster) {
@@ -16,53 +11,6 @@ if (cluster.isMaster) {
 		cluster.fork();
 	});
 
-	var today = 1;
-	var backup = function(){
-		var now = new Date();
-		if(now.getHours() == 3 && today){
-			console.log('start to backup!');
-			today = 0;
-//			var child = spawn(config.helper, ['backup'], {stdio: 'ignore'});
-			var child = spawn(config.helper, ['backup']);
-			child.stdout.on('data', function (data) {
-				console.log('stdout:', data.toString());
-			});
-			child.stderr.on('data', function (data) {
-				console.log('stderr:', data.toString());
-			});
-			child.on('close', function (code) {
-				console.log('backup end!');
-			});
-		}else{
-			if(now.getHours() == 1) today = 1;
-		}
-	}
-
-	var check_alive = function(){
-		var client = net.connect({port: 25565}, function() { //'connect' listener
-			console.log('connected to server!');
-			client.end();
-			//client.write('world!\r\n');
-		});
-		client.on('data', function(data) {
-			console.log(data.toString());
-			client.end();
-		});
-		client.on('error', function(err) {
-			console.log('o.0!', err);
-			spawn(config.helper, ['restart'], {stdio: 'ignore'});
-		});
-		client.on('end', function() {
-			console.log('disconnected from server');
-		});
-	}
-
-	var WDT = function(){
-			check_alive();
-			backup();
-			var t = setTimeout(WDT, 30*1000);
-	}
-	WDT();
 }else{
 
 	var server = require('./server.js')(config);
--- server.js
+++ server.js
@@ -5,97 +5,27 @@ module.exports = function(config){
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
-var child_process = require('child_process');
-var spawn = child_process.spawn;
 
-var tool = require('./tool.js');
 var board = require('./board.js');
 board.init();
 
 config.dir = path.join(process.cwd(), 'public');
 
-var logs = [];
-
-var exe = function(exec, arg, cb){
-	var child = spawn(exec, arg);
-	var output = '';
-	child.stdout.on('data', function (data) {
-		//console.log('stdout:', data, data.toString());
-		output += data;
-	});
-	child.on('close', function (code) {
-		//console.log('child process exited with code ' + code);
-		cb(code, output);
-	});
-}
-
-var trim_conter = 4;
-var trim = function(arr){
-	var out;
-	trim_conter++;
-	if(arr.length > 120){
-		out = arr.slice(1);
-	}else{
-		out = arr;
-	}
-	arr = null;
-	if(trim_conter == 5){
-		trim_conter = 0;
-		var last = out[out.length - 1];
-		fs.appendFile(path.join(config.dir, 'log'), JSON.stringify(last) + '\n', function (err) {
-			if (err) throw err;
-			console.log('append:', last);
-		});
-	}
-	return out;
-}
-var cpu_tool = tool.cpu(15*1000);
-var mem_tool = tool.mem(1000);
-var temp_tool = tool.temp(1000);
-var update = function(){
-	var temp = temp_tool.get();
-	var cpu = cpu_tool.get();
-	var mem = mem_tool.get();
-
-	exe(config.helper, ['tps'], function(code, data){
-		var tps = data.match(/(\d+\.\d+)/g);
-		//console.log('tps', data, tps);
-		var t = setTimeout(function(){
-			exe(config.helper, ['cmd', 'list'], function(code, data){
-// [00:41:46] [Server thread/INFO]: There are 6/36 players online:
-				//var online = data.match(/(\d+\/\d+)/g)[0].split('/');
-				var online = data.match(/\[[\d:]+\] \[Server thread\/INFO\]: There are (\d+)\/(\d+) players online:/m);
-console.log('online', data, online);
-				if(online){
-					if(online.length > 2){
-						//console.log('online', data, online);
-						//logs.push([new Date(), temp, cpu, mem, tps[0]*1.0, online[0]*1.0]);
-						logs.push([new Date(), temp, cpu, [mem.used, mem.free, mem.buffer, mem.cached], tps[0]*1.0, online[1]*1.0]);
-						logs = trim(logs);
-					}
-				}
-				var t = setTimeout(update, 60*1000);
-			});
-		}, 5*1000);
-	});
-}
-var t = setTimeout(update, 16*1000);
-
 var loadFile = function(file, ifNoneMatch, callback) {
 //console.log(file);
-  file = (file == '/') ? 'index.html': path.join(config.dir, file);
-  fs.stat(file, function(err, stat) {
-    if (err) {
-      return callback(err, null);
-    }
-    var thisEtag = '"' + stat.size + '-' + stat.mtime.getTime() + '"';
-    if (ifNoneMatch && ifNoneMatch == thisEtag) {
-      return callback(null, true, thisEtag);
-    }
-    fs.readFile(file, function(err, data) {
-      callback(err, err ? null : data, false, thisEtag);
-    });
-  });
+	file = (file == '/') ? 'index.html': path.join(config.dir, file);
+	fs.stat(file, function(err, stat) {
+		if (err) {
+			return callback(err, null);
+		}
+		var thisEtag = '"' + stat.size + '-' + stat.mtime.getTime() + '"';
+		if (ifNoneMatch && ifNoneMatch == thisEtag) {
+			return callback(null, true, thisEtag);
+		}
+		fs.readFile(file, function(err, data) {
+			callback(err, err ? null : data, false, thisEtag);
+		});
+	});
 };
 
 var server = http.createServer(handleRequest);
@@ -158,9 +88,9 @@ function handleRequest(req, res) {
 			board.handler(req, res);
 		break;
 
-		case (req.url == '/board'):
-			loadFile('board.html', req.headers['if-none-match'], send_file);
-		break;
+		/*case (req.url == '/board'):
+			loadFile('/', req.headers['if-none-match'], send_file);
+		break;*/
 
 		default:
 			loadFile(req.url, req.headers['if-none-match'], send_file);
